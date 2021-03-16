2a2604706384821950359d6047452b91502be106
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-03-16 11:04:32 +0100

    more

--- src/components/partials/navigation.js
+++ src/components/partials/navigation.js
@@ -1,10 +1,57 @@
-import React from "react";
-import { FaBell, FaSearch } from "react-icons/fa";
+import React, { useRef } from "react";
+import { FaBell, FaSearch, FaTimes } from "react-icons/fa";
 
 const Navigation = () => {
+  const navigationRef = useRef(null);
   return (
     <nav className="navigation">
-      <span className="burger">
+      <div
+        className="mobile-side-navigation-menu-container"
+        ref={navigationRef}
+      >
+        <div className="mobile-side-navigation">
+          <button
+            className="close-btn"
+            onClick={() => {
+              navigationRef.current.classList.remove("activate-sidenav");
+            }}
+          >
+            <FaTimes />
+          </button>
+          <ul>
+            <li>
+              <a href="#somewhere">Your Profile</a>
+            </li>
+            <li>
+              <a href="#somewhere">Academics</a>
+            </li>
+            <li>
+              <a href="#somewhere">Financials</a>
+            </li>
+            <li>
+              <a href="#somewhere">Health and Fitness</a>
+            </li>
+            <li>
+              <a href="#somewhere">Master Class</a>
+            </li>
+            <li>
+              <a href="#somewhere">Professional</a>
+            </li>
+            <li>
+              <a href="#somewhere">Technology</a>
+            </li>
+            <li>
+              <a href="#somewhere">Vocational</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <span
+        className="burger"
+        onClick={() => {
+          navigationRef.current.classList.add("activate-sidenav");
+        }}
+      >
         <span />
         <span />
         <span />
--- src/components/partials/recommended_topics.js
+++ src/components/partials/recommended_topics.js
@@ -3,14 +3,14 @@ import React from "react";
 const RecommendedTopics = () => {
   return (
     <div className="recommended-topics">
-      <a href="#void">Category 1</a>
-      <a href="#void">Category 2</a>
-      <a href="#void">Category 3</a>
-      <a href="#void">Category 4</a>
-      <a href="#void">Category 5</a>
-      <a href="#void">Category 3</a>
-      <a href="#void">Category 4</a>
-      <a href="#void">Category 5</a>
+      <a href="#void">Academics</a>
+      <a href="#void">Linguistics</a>
+      <a href="#void">Financials</a>
+      <a href="#void">Health and Fitness</a>
+      <a href="#void">Master Class</a>
+      <a href="#void">Professional</a>
+      <a href="#void">Technology</a>
+      <a href="#void">Vocational</a>
     </div>
   );
 };
