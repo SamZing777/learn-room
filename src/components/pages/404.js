f8237a8dc240b69dcae3464bd4ccfe5040cd099a
SamZing777 <samuelopeyemi25@gmail.com>
2021-04-08 22:35:17 +0100

    A simple boiler-plate

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound404 = () => {
   return (
     <div className="not-found-404">
-      <h1>You appear to be lost</h1>
+      <h1>Page not found! You can reach out to us.</h1>
     </div>
   );
 };
