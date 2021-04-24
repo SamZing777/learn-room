c2837811b63fae313b902df9997021abdfd15de6
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-04-24 07:54:53 +0100

    function

--- src/components/pages/jobs.js
+++ src/components/pages/jobs.js
@@ -13,47 +13,49 @@ const Jobs = () => {
           <img src={right_job} alt="find the right job" />
           <h2>Find the Right Jobs for you in Nigeria and Africa</h2>
         </div>
-        <div className="job-search-filter">
-          <form>
-            <div className="input-container">
-              <select>
-                <option disabled selected>
-                  Jobs Specialization
-                </option>
-                <option>Category a</option>
-                <option>Category b</option>
-                <option>Category c</option>
-                <option>Category d</option>
-              </select>
+        <div className="job-search-container">
+          <div className="job-search-filter">
+            <form>
+              <div className="input-container">
+                <select>
+                  <option disabled selected>
+                    Jobs Specialization
+                  </option>
+                  <option>Category a</option>
+                  <option>Category b</option>
+                  <option>Category c</option>
+                  <option>Category d</option>
+                </select>
+              </div>
+              <div className="input-container">
+                <select>
+                  <option disabled selected>
+                    All Industries
+                  </option>
+                  <option>Category a</option>
+                  <option>Category b</option>
+                  <option>Category c</option>
+                  <option>Category d</option>
+                </select>
+              </div>
+              <div className="input-container">
+                <select>
+                  <option disabled selected>
+                    All Locations
+                  </option>
+                  <option>Category a</option>
+                  <option>Category b</option>
+                  <option>Category c</option>
+                  <option>Category d</option>
+                </select>
+              </div>
+            </form>
+          </div>
+          <div className="top-jobs">
+            <h2 style={{ padding: "15px 0", textAlign: "center" }}>Top Jobs</h2>
+            <div className="jobs-listing">
+              <JobLists />
             </div>
-            <div className="input-container">
-              <select>
-                <option disabled selected>
-                  All Industries
-                </option>
-                <option>Category a</option>
-                <option>Category b</option>
-                <option>Category c</option>
-                <option>Category d</option>
-              </select>
-            </div>
-            <div className="input-container">
-              <select>
-                <option disabled selected>
-                  All Locations
-                </option>
-                <option>Category a</option>
-                <option>Category b</option>
-                <option>Category c</option>
-                <option>Category d</option>
-              </select>
-            </div>
-          </form>
-        </div>
-        <div className="top-jobs">
-          <h2 style={{ padding: "15px 0", textAlign: "center" }}>Top Jobs</h2>
-          <div className="jobs-listing">
-            <JobLists />
           </div>
         </div>
       </div>
