611a7f789ce7d3caaca2630634030a480055149f
Samuel <55145842+SamZing777@users.noreply.github.com>
2021-04-24 14:05:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Login from "./components/pages/auth pages/login";
 import Signup from "./components/pages/auth pages/signup";
 import Home from "./components/pages/home";
 import PreviewView from "./components/pages/preview_course";
-// import LiveClass from "./components/pages/live_class";
-import CourseCategory from "./components/pages/category";
-import AddCourse from "./components/pages/create_course";
-import Jobs from "./components/pages/jobs";
-
 import "./styles/base.css";
 import "./styles/style1.css";
 import "./styles/auth.css";
 import "./styles/preview_course.css";
 import "./styles/style2.css";
+import "./styles/style3.css";
+import "./styles/media-query1.css";
+import "./styles/media-query2.css";
+import LiveClass from "./components/pages/live_class";
+import CourseCategory from "./components/pages/category";
+import AddCourse from "./components/pages/create_course";
+import Jobs from "./components/pages/jobs";
+import ClassRoom from "./components/pages/class";
+import WhyChooseUs from "./components/pages/why_choose_us";
 
 function App() {
   return (
@@ -23,9 +27,12 @@ function App() {
         <Route path="/auth/signup" exact component={Signup} />
         <Route path="/auth/login" exact component={Login} />
         <Route path="/preview" exact component={PreviewView} />
+        <Route path="/live_class" exact component={LiveClass} />
         <Route path="/category" exact component={CourseCategory} />
         <Route path="/add_course" exact component={AddCourse} />
         <Route path="/jobs" exact component={Jobs} />
+        <Route path="/classroom" exact component={ClassRoom} />
+        <Route path="/why_choose_us" exact component={WhyChooseUs} />
         <Route component={NotFound404} />
       </Switch>
     </Router>
@@ -33,5 +40,3 @@ function App() {
 }
 
 export default App;
-
-// <Route path="/live_class" exact component={LiveClass} />
