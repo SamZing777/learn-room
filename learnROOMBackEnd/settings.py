a662ba2e677cc292d83085e66950b75f8da7c056
Samuel <55145842+SamZing777@users.noreply.github.com>
2021-04-28 18:09:32 +0100

    Cors origin allow all

--- learnROOMBackEnd/settings.py
+++ learnROOMBackEnd/settings.py
@@ -100,14 +100,17 @@ SWAGGER_SETTINGS = {
     'LOGOUT_URL': 'rest_framework:logout'
 }
 
-
+"""
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
     'http://localhost:4200',
     'http://localhost:3000',
     '*'
 )
+"""
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
