276232cecf20dcad5179e198ada919c23235e843
Samuel <55145842+SamZing777@users.noreply.github.com>
2021-04-28 17:46:05 +0100

    Cors origin for local development testing

--- learnROOMBackEnd/settings.py
+++ learnROOMBackEnd/settings.py
@@ -103,7 +103,9 @@ SWAGGER_SETTINGS = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
-    'http://localhost:4200'
+    'http://localhost:4200',
+    'http://localhost:3000',
+    '*'
 )
 
 
